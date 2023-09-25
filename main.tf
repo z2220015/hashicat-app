e56b77063b03974439a75b06eb4b8a248e7b2d96
z2220015 <aoi.inoue@ctc-g.co.jp>
2023-09-25 07:10:57 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -197,8 +197,3 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
-
-module "s3-bucket" {
-  source  = "cloudposse/s3-bucket/aws"
-  version = "3.0.0"
-}
